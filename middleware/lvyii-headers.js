6b9b35d6575b904dff61da852f7e9fde5c02a957
yang24201 <yang24201@126.com>
2017-12-22 10:40:33 +0800

    add header validation and import lvyii_storage

---
+++ middleware/lvyii-headers.js
@@ -0,0 +1,85 @@
+var crypto = require('crypto');
+
+var utils = require('../lib/utils');
+
+module.exports = function(LY) {
+  return function(options) {
+    options = options || {};
+
+    return function(req, res, next) {
+      var appId, appKey, contentType, param, prod, prodHeader, prodValue, sessionToken;
+      contentType = req.headers['content-type'];
+      if (/^text\/plain.*/i.test(contentType)) {
+        if (req.body && req.body !== '') {
+          req.body = JSON.parse(req.body);
+        }
+        appId = req.body._ApplicationId;
+        appKey = req.body._ApplicationKey;
+        prodValue = req.body._ApplicationProduction;
+        sessionToken = req.body._SessionToken;
+        for (param in req.body) {
+          // remove _* but keep __*
+          if (param.charAt(0) === '_' && param.charAt(1) !== '_') {
+            delete req.body[param];
+          }
+        }
+        prod = 1;
+        if (prodValue === 0 || prodValue === false) {
+          prod = 0;
+        }
+        req.LY = {
+          id: appId,
+          key: appKey,
+          prod: prod,
+          sessionToken: sessionToken
+        };
+      } else {
+        appId = req.headers['x-lc-id'];
+        appKey = req.headers['x-lc-key'];
+        prodHeader = req.headers['x-lc-prod'];
+        sessionToken = req.headers['x-lc-session'];
+        prod = 1;
+        if (prodHeader === '0' || prodHeader === 'false') {
+          prod = 0;
+        }
+        req.sessionToken = sessionToken;
+        req.LY = {
+          id: appId,
+          key: appKey,
+          prod: prod,
+          sessionToken: sessionToken
+        };
+      }
+
+      if (options.restrict) {
+        var key, requestSign, sign, timestamp, validSign, _ref;
+        if (!req.LY.id) {
+          return utils.unauthResp(res);
+        }
+        if (LY.applicationId === req.LY.id &&
+            LY.applicationKey === req.LY.key) {
+          return next();
+        }
+        requestSign = req.headers['x-lc-sign'];
+        if (requestSign) {
+          _ref = requestSign.split(',');
+          sign = _ref[0];
+          timestamp = _ref[1];
+          key = LY.applicationKey;
+          validSign = signByKey(timestamp, key);
+          if (validSign === sign.toLowerCase()) {
+            req.LY.key = key;
+            return next();
+          }
+        }
+        return utils.unauthResp(res);
+      } else {
+        return next();
+      }
+    };
+  }
+}
+
+function signByKey(timestamp, key) {
+  return crypto.createHash('md5').update('' + timestamp + key).digest('hex');
+}
